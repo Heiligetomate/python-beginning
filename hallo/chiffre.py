aebbc5dc83b5e54feacb004fd8111aca7b4d75c6
Heiligetomate <166439476+Heiligetomate@users.noreply.github.com>
2024-04-15 14:00:23 +0200

    Aktualisieren von chiffre.py

--- hallo/chiffre.py
+++ hallo/chiffre.py
@@ -1,61 +1,41 @@
 import sys
 
 
+def chiffre(a):
+    text = input("Welchen Text? ")
+    text = text.lower()
+    text = text.replace(" ", "")
+    text = list(text)
+    key_word = input("Welches Key Wort? ")
+    key = list(key_word)
+    for i in range(int(len(text) / len(key_word))):
+        for i in key_word:
+            key.append(i)
+
+    finish = []
+    for i in range(len(text)):
+        element = ord(text[i])
+        result = element - ord(key[i]) - a
+        if a == 97:
+            result = element + ord(key[i]) - a
+        if result > 122:
+            result = result - 26
+        result = chr(result)
+        finish.append(result)
+
+    finish = "".join(finish)
+    print(f"Die Übersetzung lautet: {finish}.")
+
+
 while True:
+
+    print("Was wollen sie machen? ")
     user_input = input("[1] : verschlüsseln\n[2] : entschlüsseln\n[3] : leaven\n")
     if user_input == "2":
-        # ZUM ENTSCHLÜSSELN
-        text = input("Welchen Text? ")
-        text = text.lower()
-        text = text.replace(" ", "")
-        text = list(text)
-        digit = input("Welches Wort? ")
-        key = list(digit)
-        len_text = len(text)
-        len_digit = len(digit)
-        for i in range(int(len_text / len_digit)):
-            for i in digit:
-                key.append(i)
-
-        finish = []
-        for i in range(len_text):
-            add = ord(key[i])
-
-            element = ord(text[i])
-            result = element - add + 123
-            if result > 122:
-                result = result - 26
-            result = chr(result)
-            finish.append(result)
-        finish = "".join(finish)
-        print(f"Die Übersetzung lautet: {finish}.")
+        chiffre(-123)
 
     elif user_input == "1":
-        text = input("welchen Text? ")
-        text = text.lower()
-        text = text.replace(" ", "")
-        text = list(text)
-        digit = input("Welches Key_word? ")
-        key = list(digit)
-        len_text = len(text)
-        len_digit = len(digit)
-        for i in range(int(len_text / len_digit)):
-            for i in digit:
-                key.append(i)
-
-        finish = []
-        for i in range(len_text):
-            value = ord(key[i])
-            value = value - 97
-            element = ord(text[i])
-            result = element + value
-            if result > 122:
-                result = result - 26
-            result = chr(result)
-            finish.append(result)
-
-        finish = "".join(finish)
-        print(f"Die Übersetzung lautet: {finish}.")
+        chiffre(97)
 
     elif user_input == "3":
         print("Bye Bye")
